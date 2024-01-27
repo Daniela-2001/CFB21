8e8af3050a0868fbcf8858ca40218ad6be02101a
Daniela Duarte <danielatavaresd@gmail.com>
2024-01-27 10:50:53 +0000

    CFB21

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿namespace aula21
+{
+    internal class Program
+    {
+        static void Main() 
+        {
+            string senha = "123";
+            string senhauser;
+            int tentativas = 0;
+
+            do
+            {
+                Console.Clear();
+                Console.WriteLine("Digite a senha: ");
+                senhauser= Console.ReadLine();
+                tentativas++;
+            } while(senhauser != senha);
+
+            Console.Clear(); 
+            Console.WriteLine("Senha correta, tentativas:{0}", tentativas);
+        }
+    }
+}
